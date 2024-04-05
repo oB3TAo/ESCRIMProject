6fcb80177dc11371f113cc0d72f4dfd04b3433dc
oB3TAo <loic-laine@orange.fr>
2024-04-05 17:06:26 +0200

    Creating the CRUD application to start creating the real project

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -10,6 +10,7 @@ module com.example.escrimproject {
     requires org.kordamp.bootstrapfx.core;
     requires eu.hansolo.tilesfx;
     requires com.almasb.fxgl.all;
+    requires java.sql;
 
     opens com.example.escrimproject to javafx.fxml;
     exports com.example.escrimproject;
