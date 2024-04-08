e0edd5ad817d2443b6079fd777747a07727bbb65
oB3TAo <loic-laine@orange.fr>
2024-04-08 11:31:46 +0200

    Modifying the visual elements and adding new folder

--- src/main/java/com/example/escrimproject/MaterielsController.java
+++ src/main/java/com/example/escrimproject/MaterielsController.java
@@ -9,6 +9,8 @@ import java.sql.SQLException;
 import java.util.ResourceBundle;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+
+import com.example.escrimproject.architecture.Materiel;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
@@ -19,7 +21,6 @@ import javafx.scene.control.Button;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
-import javafx.scene.control.TableRow;
 
 public class MaterielsController implements Initializable {
 
