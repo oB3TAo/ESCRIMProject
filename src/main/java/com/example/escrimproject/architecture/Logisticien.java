b072cd219dcb8dd59a6ba0bc39cd7a7063fecf31
NoeFlandre <noeflandre@gmail.com>
2024-04-28 12:19:50 +0200

    Commented Logisticien.java

--- src/main/java/com/example/escrimproject/architecture/Logisticien.java
+++ src/main/java/com/example/escrimproject/architecture/Logisticien.java
@@ -1,8 +1,17 @@
 package com.example.escrimproject.architecture;
 
+/**
+ * Represents a Logistician, a specific type of personnel within the system.
+ * This class extends the general Personnel class, specifying the type attribute for a logistician.
+ */
 public class Logisticien extends Personnel {
+
+    /**
+     * Constructor for the Logistician.
+     * Initializes a new instance of a logistician by setting its type via the constructor of the superclass.
+     */
     public Logisticien() {
-        super();
-        setType("Logisticien");
+        super(); // Call to the superclass (Personnel) constructor
+        setType("Logisticien"); // Explicitly set the type specific to this class
     }
 }
