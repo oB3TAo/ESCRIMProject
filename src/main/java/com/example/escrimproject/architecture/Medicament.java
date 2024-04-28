5cd0858151375fba1293859c7fb17706bb871e47
NoeFlandre <noeflandre@gmail.com>
2024-04-28 13:21:17 +0200

    Commented Medicament.java and removed unused elements

--- src/main/java/com/example/escrimproject/architecture/Medicament.java
+++ src/main/java/com/example/escrimproject/architecture/Medicament.java
@@ -1,20 +1,30 @@
 package com.example.escrimproject.architecture;
 
-import java.time.LocalDate;
 import java.util.Date;
 
+/**
+ * Represents a Medication product with an expiration date.
+ * Extends 'Produit' by adding a property for the expiration date of the medication.
+ */
 public class Medicament extends Produit {
+
     private Date dateDePeremption;
 
+    /**
+     * Gets the expiration date as a Date.
+     * @return the Date value of the expiration date.
+     */
     public String getDateDePeremption() {
         return String.valueOf(dateDePeremption);
     }
 
+    /**
+     * Sets the expiration date from a Date.
+     * @param dateDePeremption the Date to set as the expiration date.
+     */
     public void setDateDePeremption(Date dateDePeremption) {
         this.dateDePeremption = dateDePeremption;
     }
-    public Date dateDePeremptionProperty() {
-        return dateDePeremption;
-    }
+
 }
 
