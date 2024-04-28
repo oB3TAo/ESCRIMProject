a32a74a9bd279e33d105c9c1512aafada94d653f
NoeFlandre <noeflandre@gmail.com>
2024-04-28 13:07:18 +0200

    Commented Materiel.java

--- src/main/java/com/example/escrimproject/architecture/Materiel.java
+++ src/main/java/com/example/escrimproject/architecture/Materiel.java
@@ -3,6 +3,11 @@ package com.example.escrimproject.architecture;
 import javafx.beans.property.SimpleStringProperty;
 import javafx.beans.property.StringProperty;
 
+/**
+ * Represents a Material type of Product in the system.
+ * This class extends Produit, specifying that it represents physical, tangible items.
+ */
+
 public class Materiel extends Produit{
 
 }
